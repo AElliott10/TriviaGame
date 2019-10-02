0a0fbcefc57ba8963f0950ee2f793f58e0a2a319
Chingford <chingford162@outlook.com>
2019-10-02 18:18:45 -0400

    finalgame

---
+++ assets/javascript/jsquestion.js
@@ -0,0 +1,31 @@
+var quizQuestions = [
+    {
+        question: "What year did Temple of Dome hit theatres?",
+        choices: ["1981", "1984", "1994", "1991"],
+        correctAnswer: "1981"
+    },
+
+    {
+        question: "Who played Indiana Jones?",
+        choices: ["Tom Cruise", "Michael Douglas", "Harrison Ford", "Bruce Willis"],
+        correctAnswer: "Harrison Ford"
+    },
+
+    {
+        question: "What country are Moses tablets buried?",
+        choices: ["Jordan", "Israel", "Egypt", "Iran"],
+        correctAnswer: "Egypt"
+    },
+];
+
+var celebrateImages = [
+   'assets\images\got it right.gif',
+   'assets\images\got it right2.gif',
+   'assets\images\got it right3.gif',
+];
+
+var oopsImages = [
+    '.assets\images\ooops.gif',
+    '.assets\images\ooops2.gif',
+    '.assets\images\ooops3.gif',
+ ];
